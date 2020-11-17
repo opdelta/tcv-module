b6575e2819d917cb5b4aa1325a6e06ead7bd7b80
opdelta <ziad.ltf@gmail.com>
2020-11-17 13:02:20 -0500

    Ajout des fonctions des transactions en entrÉe

--- malib.c
+++ malib.c
@@ -1,2 +1,66 @@
 //malib.c
 #include "malib.h"
+#include <stdio.h>
+
+Id* getIdentification(int _timestamp, int _signature, float _id, unsigned char _emetteurPow) {
+
+    if (_id == 0) {
+        _id = 9999;
+    }
+    if (_emetteurPow == 0) {
+        _emetteurPow = 2;
+    }
+
+    Id *identification;
+    identification->timestamp = _timestamp;
+    identification->signature = _signature;
+    identification->id = _id;
+    identification->emetteurPow = _emetteurPow;
+
+    return identification;
+}
+
+TemperatureH* getTemperatureH(int _timestamp, int _signature, float _temp, int _error) {
+    TemperatureH *tempH;
+    tempH->timestamp = _timestamp;
+    tempH->signature = _signature;
+    if (_error == 0) {
+        tempH->temp = _temp;
+    }
+
+    return tempH;
+}
+
+TemperatureA* getTemperatureA(int _timestamp, int _signature, float _temp, int _error) {
+    TemperatureA *tempA;
+    tempA->timestamp = _timestamp;
+    tempA->signature = _signature;
+    if (_error == 0) {
+        tempA->temp = _temp;
+    }
+
+    return tempA;
+}
+
+Pulsation* getPulsation(int _timestamp, int _signature, float _pulse, int _error) {
+    Pulsation *pulseParM;
+    pulseParM->timestamp = _timestamp;
+    pulseParM->signature = _signature;
+    if (_error == 0) {
+        pulseParM->pulse = _pulse;
+    }
+   
+   return pulseParM;
+}
+
+EchangeDonnees* getEchangeDonnees(int _timestamp, int _signature, int _id, int _idpn []) {
+    EchangeDonnees *data;
+    data->timestamp = _timestamp;
+    data->signature = _signature;
+    data->id = _id;
+    int *idpnDynamic [] = malloc(sizeof(_idpn)*sizeof(int));
+
+    data->idpn = *idpnDynamic;
+
+    return data;
+}
